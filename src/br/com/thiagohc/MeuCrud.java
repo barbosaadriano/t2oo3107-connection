4a82a8fc6d2480ffe0962e8a54154e1891a6044d
Thiago <thiago_crestani@hotmail.com>
2018-07-31 22:33:10 -0300

    Merge

---
+++ src/br/com/thiagohc/MeuCrud.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.thiagohc;
+
+import br.com.adrianob.CrudInterface;
+
+/**
+ *
+ * @author thiag
+ */
+public class MeuCrud implements CrudInterface {
+
+    @Override
+    public void retriveAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insert() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
