f72325e0c0770cf6467636e1992ab955ee3a888c
Thiago <thiago_crestani@hotmail.com>
2018-08-07 22:10:26 -0300

    Trabalho

--- src/br/com/thiagohc/MeuCrud.java
+++ src/br/com/thiagohc/MeuCrud.java
@@ -6,6 +6,12 @@
 package br.com.thiagohc;
 
 import br.com.adrianob.CrudInterface;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,25 +19,63 @@ import br.com.adrianob.CrudInterface;
  */
 public class MeuCrud implements CrudInterface {
 
+    private Connection conn;
+
+    public MeuCrud(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void retriveAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            PreparedStatement ps = this.conn.prepareStatement("select * from pessoa");
+            ResultSet res = ps.executeQuery();
+
+            while (res.next()) {
+                String nome = res.getString("nome");
+                System.out.println(nome);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 
     @Override
     public void insert() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            for (int i = 1; i < 6; i++) {
+                PreparedStatement ps = conn.prepareStatement("insert into pessoa(nome) values('Thiago" + i + "');");
+                ps.executeUpdate();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(MeuCrud.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void update() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement("update pessoa set nome = 'João' Where nome = 'Thiago1'");
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(MeuCrud.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     @Override
     public void delete() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement("delete from pessoa where id = 1;");
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(MeuCrud.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
-    
+
 }
